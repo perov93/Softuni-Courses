4d2a41a87883bba1f4b8db1442a08b0508b8a18d
Perov <pavelperov93@gmail.com>
2018-03-08 15:52:36 +0200

    update

---
+++ JSCORE-2018/Advanced/Inheritance and Prototypes/03. Extending Prototype.js
@@ -0,0 +1,61 @@
+function solve() {
+
+    class Person {
+        constructor(name, email) {
+            this.name = name;
+            this.email = email;
+        }
+
+        toString() {
+            let className = this.constructor.name;
+            return `${className} (name: ${this.name}, email: ${this.email})`
+        }
+    }
+
+    class Teacher extends Person {
+        constructor(name, email, subject) {
+            super(name, email)
+            this.subject = subject;
+        }
+
+        toString() {
+            let base = super.toString().slice(0, -1)
+            return base + `, subject: ${this.subject})`
+        }
+    }
+
+    class Student extends Person {
+        constructor(name, email, course) {
+            super(name, email)
+            this.course = course;
+        }
+
+        toString() {
+            let base = super.toString().slice(0, -1)
+            return base + `, course: ${this.course})`
+        }
+    }
+
+    Person.prototype.species = 'Human';
+    Person.prototype.toSpeciesString = function () {
+        return `I am ${this.species}. ${this.toString()}`
+    }
+    return{
+        Person,
+        Teacher,
+        Student
+    }
+}
+
+let person = new Person('Pavel', 'pavelperov93@gmail.com')
+console.log(person.toString());
+
+let teacher = new Teacher('Viktor Kostadinov', 'vk@yahoo.com', 'JavaScript')
+console.log(teacher.toString());
+
+let student = new Student('Someone', 'sm@yahoo.com', 'C#')
+console.log(student.toString());
+
+console.log(person.toSpeciesString());
+console.log(teacher.toSpeciesString());
+console.log(student.toSpeciesString());
