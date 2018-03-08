5984ad9b88a837edbadfad4d35916962bf47bddf
Perov <pavelperov93@gmail.com>
2018-03-08 15:31:24 +0200

    update

--- JSCORE-2018/Advanced/Inheritance and Prototypes/02. Inheriting and Replacing ToString.js
+++ JSCORE-2018/Advanced/Inheritance and Prototypes/02. Inheriting and Replacing ToString.js
@@ -1,43 +1,36 @@
-function inheritingAndReplacingToString() {
-    class Person {
-        constructor(name, email) {
-            this.name = name;
-            this.email = email;
-        }
-
-        toString() {
-            let className = this.constructor.name;
-            return `${className} (name: ${this.name} email: ${this.email})`
-        }
+class Person {
+    constructor(name, email) {
+        this.name = name;
+        this.email = email;
     }
 
-    class Teacher extends Person {
-        constructor(name, email, subject) {
-            super(name, email)
-            this.subject = subject;
-        }
+    toString() {
+        let className = this.constructor.name;
+        return `${className} (name: ${this.name}, email: ${this.email})`
+    }
+}
 
-        toString() {
-            let base = super.toString().slice(0, -1)
-            return base + `, subject: ${this.subject}`
-        }
+class Teacher extends Person {
+    constructor(name, email, subject) {
+        super(name, email)
+        this.subject = subject;
     }
 
-    class Student extends Person {
-        constructor(name, email, course) {
-            super(name, email)
-            this.course = course;
-        }
-
-        toString() {
-            let base = super.toString().slice(0, -1)
-            return base + `, course: ${this.course}`
-        }
+    toString() {
+        let base = super.toString().slice(0, -1)
+        return base + `, subject: ${this.subject})`
     }
-    return{
-        Person,
-        Teacher,
-        Student
+}
+
+class Student extends Person {
+    constructor(name, email, course) {
+        super(name, email)
+        this.course = course;
+    }
+
+    toString() {
+        let base = super.toString().slice(0, -1)
+        return base + `, course: ${this.course})`
     }
 }
 
