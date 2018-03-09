cb11b5d0085e10816e863b3738d0d24353ccfbf2
Perov <pavelperov93@gmail.com>
2018-03-10 00:23:49 +0200

    update

--- JSCORE-2018/Advanced/Inheritance and Prototypes/04. Class Hierarchy.js
+++ JSCORE-2018/Advanced/Inheritance and Prototypes/04. Class Hierarchy.js
@@ -1,48 +1,46 @@
-function solve() {
-    class Figure{
-        constructor(){
-            if(new.target === Figure){
-                throw  new TypeError('Invalid type')
-            }
+class Figure {
+    constructor() {
+        if (new.target === Figure) {
+            throw  new TypeError('Invalid type')
         }
     }
 
-    class Circle extends Figure{
-        constructor(radius){
-            super();
-            this.radius = radius;
-        }
-
-        get area(){
-            return Math.PI * this.radius * this.radius
-        }
-
-        toString(){
-            return `Circle - radius: ${this.radius}`
-        }
-    }
-
-    class Rectangle extends Figure{
-        constructor(width, height){
-            super();
-            this.width = width;
-            this.height = height;
-        }
-
-        get area(){
-            return this.width * this.height;
-        }
-
-        toString(){
-            return `Rectangle - width: ${this.width}, height: ${this.height}`
-        }
-    }
-    return {
-        Figure,
-        Circle,
-        Rectangle
+    toString() {
+        return this.constructor.name;
     }
 }
+
+class Circle extends Figure {
+    constructor(radius) {
+        super();
+        this.radius = radius;
+    }
+
+    get area() {
+        return Math.PI * this.radius * this.radius
+    }
+
+    toString() {
+        return `${super.toString()} - radius: ${this.radius}`
+    }
+}
+
+class Rectangle extends Figure {
+    constructor(width, height) {
+        super();
+        this.width = width;
+        this.height = height;
+    }
+
+    get area() {
+        return this.width * this.height;
+    }
+
+    toString() {
+        return `${super.toString()} - width: ${this.width}, height: ${this.height}`
+    }
+}
+
 
 let ci = new Circle(5);
 console.log(ci.toString());
